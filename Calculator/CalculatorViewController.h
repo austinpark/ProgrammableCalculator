c2ad56b1b8e544eb2e05915780871c465ec92ec7
Austin <austin@administrators-Mac.local>
2012-10-30 13:26:04 -0400

    Initial Commit

---
+++ Calculator/CalculatorViewController.h
@@ -0,0 +1,13 @@
+//
+//  CalculatorViewController.h
+//  Calculator
+//
+//  Created by Austin on 10/30/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CalculatorViewController : UIViewController
+
+@end
